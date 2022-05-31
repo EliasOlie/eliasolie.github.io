31bf311f54dd80a3970be5b04388a93963707723
EliasOlie <eliasvieira31@gmail.com>
2022-05-31 18:53:22 -0300

    Added startup messages and removed some comments

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const form = document.forms.namedItem("cmd").addEventListener("submit", ((e) => 
         commands[e.target.command.value](e)
 
     } catch (error) {
-        res.style.display === "none" ? res.style.display = "block" : res.style.display = "block"
         res.innerHTML += `bash: ${e.target.command.value}: comando não encontrado <br/>`
         res.innerHTML += "Comando desconhecido, use 'help' para acessar a lista de comandos <br/>"
         e.target.command.value = ""
@@ -22,7 +21,6 @@ const form = document.forms.namedItem("cmd").addEventListener("submit", ((e) => 
 
 const acceptedCommands = {
     handleHelp(e) {
-        res.style.display === "none" ? res.style.display = "block" : res.style.display = "block"
         res.innerHTML += e.target.command.value + "<br/>"
         const commandsDesc = [{
                 name: "help",
@@ -52,7 +50,6 @@ const acceptedCommands = {
 
     },
     handlePwd(e) {
-        res.style.display === "none" ? res.style.display = "block" : res.style.display = "block"
         res.innerHTML += e.target.command.value + "<br/>"
         res.innerHTML += "~/ <br/>"
         e.target.command.value = ""
@@ -64,7 +61,6 @@ const acceptedCommands = {
     },
     handleWhoAmI(e) {
         //Formatar melhor a saída de texto
-        res.style.display === "none" ? res.style.display = "block" : res.style.display = "block"
         res.innerHTML += e.target.command.value + "<br/>"
 
         response = [
@@ -76,7 +72,7 @@ const acceptedCommands = {
             "O tempo foi passando e eu fui voltando a ter o desejo de aprender programação, e várias outras coisas que havia tentado, mas devido a inconstância",
             "não havia conseguido nada, começando no violão fui me dedicando e posteriormente no programa de Jovem Aprendiz onde pude exercer a atividade de",
             "auxiliar administrativo, mexendo nas planilhas do Excel conheci o VBA e que baque quando consegui fazer algumas automações simples com o VBA",
-            "Ddepois disso me aventurei no python através do livro 'Curso intensivo de python do Eric Matthes' passei do 'hello world' e decolei",
+            "depois disso me aventurei no python através do livro 'Curso intensivo de python do Eric Matthes' passei do 'hello world' e decolei",
             "até hoje venho me aprofundando em vários temas desde DevOPS, arquitetura até ML e IA",
             "Atualmente consigo tocar violão, cavaquinho, pandeiro, bateria e teclado (como? sendo péssimo em todos!!! XD) e tenho desenvolvido principalmente",
             "Em python, Typescript, C++ e Ruby. Estou num momento no qual me julgo preparado para participar desse mercado tão disputado e feroz da computação",
@@ -99,4 +95,55 @@ const commands = {
     "pwd": acceptedCommands.handlePwd,
     "cls": acceptedCommands.handleCls,
     "whoami": acceptedCommands.handleWhoAmI
+
+
 }
+
+//Startup session
+
+window.onload = ((e) => {
+    const results = document.getElementById("console")
+    results.style.display = "none"
+
+    let consoleMessages = [
+        "<div class= green>[OK]<div/><div class=white> Finished Create System Users.<div/>",
+        "<div class = white>Starting Create Static Device Nodes in /dev... <div/>",
+        "<div class= green>[OK]<div/><div class=white> Finished Create Static Device Nodes in /dev.<div/>",
+        "<div class= green>[OK]<div/><div class=white> Started Entropy Daemon based on the HAVEGE algorithm.<div/>",
+        "<div class = white>Starting Journal Service... <div/>",
+        "<div class = white>Starting Rule-based Manager for Device Events and Files... <div/>",
+        "<div class= green>[OK]<div/><div class=white> Finished Apply Kernel Variables.<div/>",
+        "<div class= green>[OK]<div/><div class=white> Started Journal Service.<div/>",
+        "<div class = white>Starting Flush Journal to Persistent Storage... <div/>",
+        "<div class= green>[OK]<div/><div class=white> Finished Flush Journal to Persistent Storage.<div/>",
+        "<div class= green>[OK]<div/><div class=white> Started Rule-based Manager for Device Events and Files.<div/>",
+        "<div class= green>[OK]<div/><div class=white> Started LVMZ metadata daemon.<div/>",
+        "<div class = white>Starting Network Service... <div/>",
+        "<div class= green>[OK]<div/><div class=white> Started Network Service.<div/>",
+        "<div class = white>Starting Wait for Network to be Configured... <div/>",
+        "<div class= green>[OK]<div/><div class=white> Finished Load/Save Random Seed.<div/>",
+        "<div class=white>[ 12.472423] snd_hda_intel 0000:00:05.0: CORB reset timeout#1, CORBRP = 0 <div/>",
+        "<div class = white>Starting Wait for udev To Complete Device Initialization... <div/>",
+        "<div class= green>[OK]<div/><div class=white> Finished Wait for udev To Complete Device Initialization.<div/>",
+        "<div class= green>[OK]<div/><div class=white> Finished Monitoring of LVMZ mirrors, etc. using dmeventd or progress polling.<div/>",
+        "<div class= green>[OK]<div/><div class=white> Reached target Local File Systems (Pre).<div/>",
+        "<div class=white>Mounting Temporary /etc/pacman.d/gnupg directory... <div/>",
+        "<div class= green>[OK]<div/><div class=white> Mounted Temporary /etc/pacman.d/gnupg directory.<div/>",
+        "<div class= green>[OK]<div/><div class=white> Reached target Local File Systems.<div/>",
+        "<div class = white>Starting Rebuild Dynamic Linker Cache... <div/>",
+    ]
+
+    const startup = document.getElementById("startup")
+    consoleMessages.map((message) => {
+        setTimeout(() => {
+            startup.innerHTML += message
+        }, Math.floor(Math.random() * (2000 - 1000)) + 1000)
+    })
+
+    setTimeout(() => {
+        startup.style.display = "none"
+        results.style.display = "block"
+        res.innerHTML += "<div class=white>Dica: use 'help' para obter a lista de comandos<div/>"
+
+    }, 3000)
+})
